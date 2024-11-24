ea97cdfb9b7ec7ae0e570950216b877bf3448cc2
ksenyay <ksenyausachova@gmail.com>
2024-11-24 11:25:17 +0200

    Change html and js to implement ui logic

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,33 @@
 let playerScore = 0;
 let computerScore = 0;
 
-console.log(startGame());
+// Defining event listeners for buttons
 
-// Getting random numbers that are required for computer to make a choice
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+rockButton.addEventListener('click', () => {
+    playRound('rock');
+});
+paperButton.addEventListener('click', () => {
+    playRound('paper');
+});
+scissorsButton.addEventListener('click', () => {
+    playRound('scissors');
+});
+
+// Defining event listeners for text
+
+const playerText = document.querySelector('#player-choice');
+const computerText = document.querySelector('#computer-choice');
+
+const resultText = document.querySelector('#round-result');
+const scoreText = document.querySelector('#current-score');
+const finalResult = document.querySelector('#winner');
+
+
+// Computer makes choice
 
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
@@ -23,77 +47,57 @@ function getComputerChoice() {
     }
 } 
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
-    while (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
-        playerChoice = prompt("Please enter a valid choice: Rock, Paper or Scissors ").toLowerCase();
-    } 
-    return playerChoice;
-}
+// Round is played
 
-function playRound() {
+function playRound(player) {
+    finalResult.textContent = '';
 
-    playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
+    let playerChoice = player;
+    let computerChoice = getComputerChoice();
 
-    console.log(playerChoice);
-    console.log(computerChoice);
+    playerText.textContent = `Player choice: ${playerChoice}`;
+    computerText.textContent = `Computer choice: ${computerChoice}`;
     
     if (playerChoice === computerChoice) {
-        console.log("TIE!");
+        resultText.textContent = "TIE!";
     } else if (playerChoice == 'rock' && computerChoice == 'paper' || 
                playerChoice == 'paper' && computerChoice == 'scissors' ||
                playerChoice == 'scissors' && computerChoice == 'rock'
     ) {
-        console.log(`COMPUTER WON! ${computerChoice} beats ${playerChoice}`)
+        resultText.textContent = `COMPUTER WON! ${computerChoice} beats ${playerChoice}`;
         computerScore += 1;
     } else if (playerChoice == 'paper' && computerChoice == 'rock' ||
                playerChoice == 'scissors' && computerChoice == 'paper' ||
                playerChoice == 'rock' && computerChoice == 'scissors'
     ) {
-        console.log(`PLAYER WON! ${playerChoice} beats ${computerChoice}`);
+        resultText.textContent = `PLAYER WON! ${playerChoice} beats ${computerChoice}`;
         playerScore += 1;
-    } 
-}
-
-function startGame() {
-
-    let gamesCount = 0;
-
-    while (gamesCount < 5) {
-        console.log(playRound());
-        gamesCount += 1;
-        console.log(`The score is: Computer - ${computerScore} Player - ${playerScore}`);
     }
 
-    if (playerScore > computerScore) {
-        console.log('CONGRATULATION! PLAYER WON');
-    } else if (playerScore < computerScore) {
-        console.log('OH NO! COMPUTER WON');
+    scoreText.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
+
+    checkScore();
+}
+
+// Check score
+
+function checkScore() {
+
+    while (playerScore != 5 && computerScore != 5) {
+        quit
+    }
+
+    if (playerScore === 5) {
+        finalResult.textContent = 'CONGRATULATION! PLAYER WON';
+    } else if (computerScore === 5) {
+        finalResult.textContent = 'OH NO! COMPUTER WON';
     } else {
-        console.log("IT'S A TIE!");
+        finalResult.textContent = "IT'S A TIE!";
     }
 
     // Set  scores down to 0
 
     playerScore = 0;
     computerScore = 0;
-
-    // Start game over
-
-    console.log(playAgain());
-}
-
-function playAgain() {
-    let playerAnswer = prompt("Do you want to play again? Yes or No").toLowerCase();
-    
-    while (playerAnswer != 'yes' && playerAnswer != 'no') {
-        playerAnswer = prompt("Do you want to play again? Yes or No").toLowerCase();
-    }
-
-    if (playerAnswer =='yes') {
-        startGame();
-    } else if (playerAnswer='no') {
-        console.log("Bye!");
-    }
+    scoreText.textContent = '';
 }
